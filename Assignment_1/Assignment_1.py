2233c53e0d3d876c392aeae42983944c387ee247
mjenius <jamaledd@ualberta.ca>
2018-02-01 23:41:44 -0700

    Added MIT License

--- Assignment_1/Assignment_1.py
+++ Assignment_1/Assignment_1.py
@@ -5,6 +5,27 @@
 # The names of any students you consulted with:
 
 # Paste the license you choose for your assignment here.
+#MIT License
+
+#Copyright (c) [2018] [Mohamad Jamaleddine]
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
 
 # Instructions: write a URL parser library in Python 3. This file should contain
 # function that when called with a URL will return a 6-tuple of information
