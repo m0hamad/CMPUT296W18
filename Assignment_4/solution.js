c4f1eb05280c221af91319c8d61438ddfe062748
mjenius <jamaledd@ualberta.ca>
2018-03-25 08:01:13 -0600

    Added stuff

--- Assignment_4/solution.js
+++ Assignment_4/solution.js
@@ -1,12 +1,12 @@
 class Gambler {
 
   constructor(url, token) {
-    this.url = url
-    this.token = token
+    this.url = url;
+    this.token = token;
   }
 
   bet(amount) {
-
+    
   }
 
   stand() {
@@ -26,3 +26,18 @@ class Gambler {
   }
 
 }
+
+// #2
+fetch("https://pizza.cs.ualberta.ca/296/")
+  .then(function(response) {
+    return response.text().then(function(myJson) {
+      console.log(myJson);
+    });
+  });
+
+
+var formData = new FormData();
+formData.append('token', 'blahblahblabhalbhablah');
+//This is the line I have issues with.
+var myForm = (document.getElementsByName("session")[0]).innerHTML;
+formData = new FormData(myForm);
